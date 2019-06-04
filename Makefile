dacf4bb09deea8c2d1142c88522c7dfe417e55ac
Sahil Gandhi <sahilmgandhi@gmail.com>
2019-06-04 11:45:24 -0700

    Add conv results

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CPP=g++
 
-CFLAGS=$(OPT) --std=c++11 -O3
-MODULE          := conv1 conv2 class1 class2 cpu-conv1 cpu-conv2 cpu-class1 cpu-class2
+CFLAGS=$(OPT) --std=c++11 -O3 -w
+MODULE          := conv1 conv1c opt-conv1 opt-conv1c class1 class1c
 
 .PHONY: all clean
 
@@ -9,7 +9,6 @@ all: $(MODULE)
 
 HEADERS=dnn.hpp
 
-# These tiling parameters are 100% arbitrary, and it may be advantageous to tune/remove/completely-change them for GPU
 conv1: convolution.cu
 	/usr/local/cuda-10.1/bin/nvcc -ccbin g++ -I /home/${USER}/NVIDIA_CUDA-10.1_Samples/common/inc -m64 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75 $^ $(CFLAGS) -o $@ -DNx=256 -DNy=256 -DKx=3  -DKy=3  -DNi=128  -DNn=128  -DTii=4 -DTi=16  -DTnn=4 -DTn=16 -DTx=4 -DTy=4 -DCONCURRENT=0 -DNb=10
 
--- perf-maximize-conv.sh
+++ perf-maximize-conv.sh
@@ -2,11 +2,11 @@
 
 # conv1
 
-nx=(256)
+nx=(64 128 256 512)
 # ny=(256)
-nn=(64)
+nn=(16 32 64)
 # ni=(64)
-nb=(1 3 5 7 9 10)
+nb=(1 2 4 6 8 10 12 14 16 18 20)
 
 MAX_PARAMS=""
 MAX_PERF=0
@@ -37,9 +37,9 @@ do
             PERF2=$PERF # floating point form
             PERF=$((`printf %.0f $PERF`))
 
-            if [ $PERF -gt 0 ] && [ $PERF -lt 2000 ]
+            if [ $PERF -gt 0 ] && [ $PERF -lt 8000 ]
             then
-                echo "NX: $i NY: $i NN: $j NI: $j NB: $k => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_PARAMS"
+                echo "Sequential: NX: $i NY: $i NN: $j NI: $j NB: $k => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_PARAMS"
                 echo "$EXEC_TIME,$PERF2,$i,$i,$j,$j,$k" >> seq-batch-res.csv
             fi
 
@@ -53,34 +53,24 @@ do
             PERF2=$PERF # floating point form
             PERF=$((`printf %.0f $PERF`))
 
-            if [ $PERF -gt 0 ] && [ $PERF -lt 2000 ]
+            if [ $PERF -gt 0 ] && [ $PERF -lt 8000 ]
             then
-                echo "NX: $i NY: $i NN: $j NI: $j NB: $k => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_PARAMS"
+                echo "Concurrent: NX: $i NY: $i NN: $j NI: $j NB: $k => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_PARAMS"
                 echo "$EXEC_TIME,$PERF2,$i,$i,$j,$j,$k" >> conc-batch-res.csv
             fi
 
-            if [ $PERF -gt $MAX_PERF ] && [ $PERF -lt 2000 ]
+            if [ $PERF -gt $MAX_PERF ] && [ $PERF -lt 8000 ]
             then
                 MAX_PERF=$PERF
                 MAX_PARAMS="NX: $i NY: $i NN: $j NI: $j NB: $k"
             fi
 		done
+
+        echo "" >> conc-batch-res.csv
+        echo "" >> seq-batch-res.csv
     done
 done
 
 echo
 echo "MAX PERF => $MAX_PERF GFlops"
 echo "MAX_PARAMS => $MAX_PARAMS"
-
-
-
-# # Example sed/grep commands
-
-# cat write-profiling.txt | grep "dram_write_throughput" | sed -r "s/.*dram_write_throughput[A-Za-z ]*?([0-9]+\.[0-9]*)(KB\/s|MB\/s|GB\/s).*/\1\2/"
-# cat write-profiling.txt | grep "dram_write_transactions" | sed -r "s/.*dram_write_transactions[A-Za-z ]*?([0-9]+).*/\1/"
-# cat write-profiling.txt | grep "sysmem_write_throughput" | sed -r "s/.*sysmem_write_throughput[A-Za-z ]*?([0-9]+\.[0-9]*)(KB\/s|MB\/s|GB\/s).*/\1\2/"
-# cat write-profiling.txt | grep "sysmem_write_transactions" | sed -r "s/.*sysmem_write_transactions[A-Za-z ]*?([0-9]+).*/\1/"
-
-# cat read-profiling.txt | grep "dram_read_throughput" | sed -r "s/.*dram_read_throughput[A-Za-z ]*?([0-9]+\.[0-9]*)(KB\/s|MB\/s|GB\/s).*/\1\2/"
-# cat read-profiling.txt | grep "dram_read_transactions" | sed -r "s/.*dram_read_transactions[A-Za-z ]*?([0-9]+).*/\1/"
-# cat read-profiling.txt | grep "l2_tex_read_transactions" | sed -r "s/.*\(Texture Reads\)[A-Za-z ]*?([0-9]+).*/\1/"
