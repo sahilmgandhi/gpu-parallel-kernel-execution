cf3019da4dd7ed6e6e3f250de2c4a74ac12dedb3
Sahil Gandhi <sahilmgandhi@gmail.com>
2019-06-04 00:24:47 -0700

    Add profiling script

--- Makefile
+++ Makefile
@@ -16,5 +16,11 @@ conv1: convolution.cu
 conv1c: convolution.cu
 	/usr/local/cuda-10.1/bin/nvcc -ccbin g++ -I /home/${USER}/NVIDIA_CUDA-10.1_Samples/common/inc -m64 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75 $^ $(CFLAGS) -o $@ -DNx=256 -DNy=256 -DKx=3  -DKy=3  -DNi=128  -DNn=128  -DTii=4 -DTi=16  -DTnn=4 -DTn=16 -DTx=4 -DTy=4 -DCONCURRENT=1 -DNb=10
 
+opt-conv1: convolution.cu
+	/usr/local/cuda-10.1/bin/nvcc -ccbin g++ -I /home/${USER}/NVIDIA_CUDA-10.1_Samples/common/inc -m64 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75 $^ $(CFLAGS) -o $@ -DNx=${NX_PARAM} -DNy=${NY_PARAM} -DKx=3  -DKy=3  -DNi=${NI_PARAM} -DNn=${NN_PARAM} -DTii=4 -DTi=16 -DTnn=4 -DTn=16 -DTx=4 -DTy=4 -DCONCURRENT=0 -DNb=${NUM_BATCHES}
+
+opt-conv1c: convolution.cu
+	/usr/local/cuda-10.1/bin/nvcc -ccbin g++ -I /home/${USER}/NVIDIA_CUDA-10.1_Samples/common/inc -m64 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_75,code=compute_75 $^ $(CFLAGS) -o $@ -DNx=${NX_PARAM} -DNy=${NY_PARAM} -DKx=3  -DKy=3  -DNi=${NI_PARAM} -DNn=${NN_PARAM} -DTii=4 -DTi=16 -DTnn=4 -DTn=16 -DTx=4 -DTy=4 -DCONCURRENT=1 -DNb=${NUM_BATCHES}
+
 clean:
 	@rm -f $(MODULE) 
--- perf-maximize-conv.sh
+++ perf-maximize-conv.sh
@@ -2,105 +2,75 @@
 
 # conv1
 
-nx=(32 64 96 128 160 192 224 256)
-ny=(256)
+nx=(256)
+# ny=(256)
 nn=(64)
-ni=(64)
+# ni=(64)
+nb=(1 3 5 7 9 10)
 
-
-
-nty=(1) #might need to bump to 2, but need to change conv.cu
-nby=(32)
-ntx=(1) #might need to bump to 2, but need to change conv.cu
-nbx=(32)
-ntz=(32)
-
-MAX_tii_ti_tnn_tn_tx_ty=""
+MAX_PARAMS=""
 MAX_PERF=0
 
-echo "RUNNING TILING SCRIPT CONVOLUTION ..."
+echo "RUNNING SCRIPT TO GET VARIOUS CONVOLUTION METRICS ..."
 echo
 
-touch perf-conv-results2.csv
-echo "" > perf-conv-results2.csv
+touch conc-batch-res.csv
+echo "" > conc-batch-res.csv
 
-for x in ${nx[@]}
-do
+touch seq-batch-res.csv
+echo "" > seq-batch-res.csv
 
-for y in ${ny[@]}
-do
 
-for n in ${nn[@]}
+for i in ${nx[@]}
 do
-
-for b in ${ni[@]}
-do
-
-for i in ${nty[@]}
-do
-    for j in ${nby[@]}
+    for j in ${nn[@]}
     do
-		for k in ${ntx[@]}
+		for k in ${nb[@]}
 		do
-			for l in ${nbx[@]}
-			do
-                for m in ${ntz[@]}
-                do
-                        rm ./opt-conv1 || true
-                        make -f make-conv opt-conv1 Nx=${x} Ny=${y} Nn=${n} Ni=${b} NUM_THREADS_Y=${i} NUM_BLOCKS_Y=${j} NUM_THREADS_X=${k} NUM_BLOCKS_X=${l} NUM_THREADS_Z=${m}  2>&1 >> /dev/null
+            rm ./opt-conv1 || true
+            make opt-conv1 NX_PARAM=${i} NY_PARAM=${i} NN_PARAM=${j} NI_PARAM=${j} NUM_BATCHES=${k} 2>&1 >> /dev/null
 
-                        OUT=`./opt-conv1` 
+            OUT=`./opt-conv1` 
+            PERF=`echo $OUT | grep "GFlops (MAC=2) 1:" | sed -e 's/.*GFlops (MAC=2) 1: \(.*\) c.*/\1/'`
+            EXEC_TIME=`echo $OUT | grep "elapsed (sec):" | sed -e 's/.*elapsed (sec): \(.*\) G.*c.*/\1/'`
+            
+            PERF2=$PERF # floating point form
+            PERF=$((`printf %.0f $PERF`))
 
-                        PERF=`echo $OUT | grep "GFlops (MAC=2) 1:" | sed -e 's/.*GFlops (MAC=2) 1: \(.*\) G.*/\1/'`
-                        EXEC_TIME=`echo $OUT | grep "elapsed (sec):" | sed -e 's/.*elapsed (sec): \(.*\) G.*G.*/\1/'`
-                        
-                        PERF2=$PERF # floating point form
-                        PERF=$((`printf %.0f $PERF`))
+            if [ $PERF -gt 0 ] && [ $PERF -lt 2000 ]
+            then
+                echo "NX: $i NY: $i NN: $j NI: $j NB: $k => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_PARAMS"
+                echo "$EXEC_TIME,$PERF2,$i,$i,$j,$j,$k" >> seq-batch-res.csv
+            fi
 
-                        if [ $PERF -gt 0 ] && [ $PERF -lt 2000 ]
-                        then
-                            echo "NTY: $i NBY: $j NTX: $k NBX: $l NTZ: $m  => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_NTY_NBY_NTX_NBX_NTZ"
+            rm ./opt-conv1c || true
+            make opt-conv1c NX_PARAM=${i} NY_PARAM=${i} NN_PARAM=${j} NI_PARAM=${j} NUM_BATCHES=${k} 2>&1 >> /dev/null
 
-                            (( num_b_z = 64 / $m ))
+            OUT=`./opt-conv1c` 
+            PERF=`echo $OUT | grep "GFlops (MAC=2) 1:" | sed -e 's/.*GFlops (MAC=2) 1: \(.*\) c.*/\1/'`
+            EXEC_TIME=`echo $OUT | grep "elapsed (sec):" | sed -e 's/.*elapsed (sec): \(.*\) G.*c.*/\1/'`
+            
+            PERF2=$PERF # floating point form
+            PERF=$((`printf %.0f $PERF`))
 
-                            # ./profiling2.sh -a
+            if [ $PERF -gt 0 ] && [ $PERF -lt 2000 ]
+            then
+                echo "NX: $i NY: $i NN: $j NI: $j NB: $k => $PERF GFlops ||| Max: $MAX_PERF GFlops w/ $MAX_PARAMS"
+                echo "$EXEC_TIME,$PERF2,$i,$i,$j,$j,$k" >> conc-batch-res.csv
+            fi
 
-                            # dram_read=`cat read-profiling.txt | grep "dram_read_throughput" | sed -r "s/.*dram_read_throughput[A-Za-z ]*?([0-9]+\.[0-9]*)(KB\/s|MB\/s|GB\/s).*/\1\2/"`
-
-                            # dram_read_trans=`cat read-profiling.txt | grep "dram_read_transactions" | sed -r "s/.*dram_read_transactions[A-Za-z ]*?([0-9]+).*/\1/"`
-
-                            # l2_tex_read=`cat read-profiling.txt | grep "l2_tex_read_transactions" | sed -r "s/.*\(Texture Reads\)[A-Za-z ]*?([0-9]+).*/\1/"`
-
-                            # dram_write=`cat write-profiling.txt | grep "dram_write_throughput" | sed -r "s/.*dram_write_throughput[A-Za-z ]*?([0-9]+\.[0-9]*)(KB\/s|MB\/s|GB\/s).*/\1\2/"`
-
-                            # dram_write_trans=`cat write-profiling.txt | grep "dram_write_transactions" | sed -r "s/.*dram_write_transactions[A-Za-z ]*?([0-9]+).*/\1/"`
-
-                            # sys_write=`cat write-profiling.txt | grep "sysmem_write_throughput" | sed -r "s/.*sysmem_write_throughput[A-Za-z ]*?([0-9]+\.[0-9]*)(KB\/s|MB\/s|GB\/s).*/\1\2/"`
-
-                            # sys_write_trans=`cat write-profiling.txt | grep "sysmem_write_transactions" | sed -r "s/.*sysmem_write_transactions[A-Za-z ]*?([0-9]+).*/\1/"`
-
-                            echo "$EXEC_TIME,$PERF2,$x,$y,$n,$i,$l,$j,$num_b_z,1,$dram_read,$dram_read_trans,$l2_tex_read,$dram_write,$dram_write_trans,$sys_write,$sys_write_trans" >> perf-conv-results2.csv
-
-                        fi
-                        if [ $PERF -gt $MAX_PERF ] && [ $PERF -lt 2000 ]
-                        then
-                            MAX_PERF=$PERF
-                            MAX_NTY_NBY_NTX_NBX_NTZ="NTY: $i NBY: $j NTX: $k NBX: $l NTZ: $m"
-                        fi
-
-                done
-			done
+            if [ $PERF -gt $MAX_PERF ] && [ $PERF -lt 2000 ]
+            then
+                MAX_PERF=$PERF
+                MAX_PARAMS="NX: $i NY: $i NN: $j NI: $j NB: $k"
+            fi
 		done
     done
-done
-done
-done
-done
 done
 
 echo
 echo "MAX PERF => $MAX_PERF GFlops"
-echo "MAX_NTY_NBY_NTX_NBX_NTZ => $MAX_NTY_NBY_NTX_NBX_NTZ"
+echo "MAX_PARAMS => $MAX_PARAMS"
 
 
 
